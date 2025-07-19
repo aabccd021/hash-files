227ab309c251957ab73f5563fb14cb07724cd984
aabccd021 <aabccd021@gmail.com>
2025-07-19 10:04:00 +0700

    chore: update hash_files.sh script and add file watching option

--- hash_files.sh
+++ hash_files.sh
@@ -1,9 +1,13 @@
 input_dir=""
 output_json=""
 output_dir=""
+watch=false
 
 while [ $# -gt 0 ]; do
   case $1 in
+  --watch)
+    watch=true
+    ;;
   --input-dir)
     input_dir=$2
     shift
@@ -29,30 +33,42 @@ if [ -z "$input_dir" ]; then
   exit 1
 fi
 
-if [ ! -f "$output_json" ]; then
-  echo "{}" >"$output_json"
-fi
+while true; do
 
-asset_results=$(ls "$input_dir")
+  if [ "$watch" = true ]; then
+    inotifywait --event create "$input_dir" >/dev/null 2>&1
+  fi
 
-tmpfile=$(mktemp)
-for filename in $asset_results; do
-  filename_no_ext="${filename%.*}"
-  ext="${filename##*.}"
-  hash=$(md5sum "$input_dir/$filename" | cut -d ' ' -f 1)
-  hash_filename="$filename_no_ext.$hash.$ext"
+  if [ ! -f "$output_json" ]; then
+    echo "{}" >"$output_json"
+  fi
 
-  if [ -n "$output_json" ]; then
-    jq ".\"$filename\" = \"$hash_filename\"" "$output_json" >"$tmpfile"
+  asset_results=$(ls "$input_dir")
 
-    # avoid unnecessary write by doing it only if output_json is different from tmpfile
-    if ! cmp -s "$output_json" "$tmpfile"; then
-      truncate -s 0 "$output_json"
-      cat "$tmpfile" >>"$output_json"
+  tmpfile=$(mktemp)
+  for filename in $asset_results; do
+    filename_no_ext="${filename%.*}"
+    ext="${filename##*.}"
+    hash=$(md5sum "$input_dir/$filename" | cut -d ' ' -f 1)
+    hash_filename="$filename_no_ext.$hash.$ext"
+
+    if [ -n "$output_json" ]; then
+      jq ".\"$filename\" = \"$hash_filename\"" "$output_json" >"$tmpfile"
+
+      # avoid unnecessary write by doing it only if output_json is different from tmpfile
+      if ! cmp -s "$output_json" "$tmpfile"; then
+        truncate -s 0 "$output_json"
+        cat "$tmpfile" >>"$output_json"
+      fi
     fi
+
+    if [ -n "$output_dir" ]; then
+      mv "$input_dir/$filename" "$output_dir/$hash_filename"
+    fi
+  done
+
+  if [ "$watch" = false ]; then
+    break
   fi
 
-  if [ -n "$output_dir" ]; then
-    mv "$input_dir/$filename" "$output_dir/$hash_filename"
-  fi
 done
